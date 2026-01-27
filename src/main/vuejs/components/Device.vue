685bdafd8696a34887807ae475e0799c1ce34066
Jermaine Sepkitt <Sepkitt.io@gmail.com>
2026-01-27 15:01:32 +0200

    Created the device component

---
+++ src/main/vuejs/components/Device.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="wrapper" :style="`transform:{};`">
+      <v-sheet elevation="10" class="device" color="red" :height="height" :width="width" >
+        <iframe id="frame_1" class="holds-the-iframe"  :src="src" />
+      
+      </v-sheet>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+   activeDevice: { type: Object },
+    height: {
+      required: false,
+      default:480
+    },
+    width: {
+      required: false,
+      default:740
+
+    },
+    screen: { type: String },
+    src: {
+      type: String,
+      default: 'https://nuxtjs.org/',
+    },
+    deviceType: {
+      type: String,
+    },
+    types: {
+      type: Array,
+    },
+})
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main/vuejs/pages/ui/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+  <Device/>
+
+  </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
